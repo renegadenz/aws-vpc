ab5c198f67463d79d2fe6c3f56e7efab6db70d95
james_mead <james.mead@sky.co.nz>
2024-10-22 21:27:03 +0000

    corrected outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,24 @@
-variable "name" {
-  description = "Name prefix for all resources"
-  type        = string
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.this.id
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+output "dmz_subnet_ids" {
+  description = "List of IDs of DMZ subnets"
+  value       = aws_subnet.dmz[*].id
 }
 
-variable "availability_zones" {
-  description = "List of availability zones to use"
-  type        = list(string)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+output "logic_subnet_ids" {
+  description = "List of IDs of Logic subnets"
+  value       = aws_subnet.logic[*].id
 }
 
-variable "tags" {
-  description = "A map of tags to add to all resources"
-  type        = map(string)
-  default     = {}
+output "data_subnet_ids" {
+  description = "List of IDs of Data subnets"
+  value       = aws_subnet.data[*].id
+}
+
+output "nat_gateway_ids" {
+  description = "List of NAT Gateway IDs"
+  value       = aws_nat_gateway.this[*].id
 }
